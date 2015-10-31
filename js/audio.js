bf9447474d78a9cde854be57146d4f17b075ce8a
Alex Kass <alexrkass@gmail.com>
2015-10-30 21:43:10 -0700

    add working audio pipeline

--- js/audio.js
+++ js/audio.js
@@ -1,43 +1,35 @@
 (function() {
-  var audio, request, url;
+  var audio, request, url, analyser, source, sourceJs, visualisercontainer, playbuttoncontainer;
 
-  try {
-    audio = new AudioContext();
-  } catch (_error) {}
-
+  //FIRST, CREATE AUDIO PIPELINE AND GRAB MUSIC
+  audio = new AudioContext();
   url = 'data/music.mp3';
-
   request = new XMLHttpRequest();
 
   request.open('GET', url, true);
-
   request.responseType = "arraybuffer";
 
   request.onload = function() {
-    return audio.decodeAudioData(request.response).then(function(buffer) {
-      var analyser, source, sourceJs;
+     audio.decodeAudioData(request.response, function(decodedData) {
       source = audio.createBufferSource();
-      source.buffer = buffer;
+      source.buffer = decodedData;
       source.connect(audio.destination);
-      sourceJs = audio.createScriptProcessor(2048, 1, 1);
-      sourceJs.buffer = buffer;
+
+      sourceJs = audio.createScriptProcessor(0, 1, 1); //buffersize (0 is default), input channels, output channels
+      sourceJs.buffer = decodedData;
+
       analyser = audio.createAnalyser();
       analyser.smoothingTimeConstant = 1;
       analyser.fftSize = 512;
+
       source.connect(analyser);
-      analyser.connect(sourceJs);
-      analyser.connect(myVisualizer);
+
       sourceJs.onaudioprocess = function(e) {
-        var array;
-        array = new Uint8Array(analyser.frequencyBinCount);
-        return analyser.getByteFrequencyData(array);
+        var array = new Uint8Array(analyser.fftSize);
+        analyser.getByteFrequencyData(array);
       };
-      source.loop = true;
     });
   };
-
   request.send();
-
-  return audio;
 
 }).call(this);
